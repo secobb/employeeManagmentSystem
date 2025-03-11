5e0d2677efa21c5cf6c23f26d8871fd331abef9e
secobb <design.serg@gmail.com>
2025-03-11 15:49:38 +0200

    dashboard controller update

--- src/main/java/com/employeemanagmentsystem/employeemanagmentsystem/DashboardController.java
+++ src/main/java/com/employeemanagmentsystem/employeemanagmentsystem/DashboardController.java
@@ -1,4 +1,160 @@
 package com.employeemanagmentsystem.employeemanagmentsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+
+    @FXML
+    private Button addEmployee_addBtn;
+
+    @FXML
+    private Button addEmployee_btn;
+
+    @FXML
+    private Button addEmployee_clearBtn;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_date;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_employeeID;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_firstName;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_gender;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_lastName;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_phoneNum;
+
+    @FXML
+    private TableColumn<?, ?> addEmployee_col_position;
+
+    @FXML
+    private Button addEmployee_deleteBtn;
+
+    @FXML
+    private TextField addEmployee_employeeID;
+
+    @FXML
+    private TextField addEmployee_firstName;
+
+    @FXML
+    private AnchorPane addEmployee_form;
+
+    @FXML
+    private ComboBox<?> addEmployee_gender;
+
+    @FXML
+    private ImageView addEmployee_image;
+
+    @FXML
+    private Button addEmployee_importBtn;
+
+    @FXML
+    private TextField addEmployee_lastName;
+
+    @FXML
+    private TextField addEmployee_phoneNum;
+
+    @FXML
+    private ComboBox<?> addEmployee_position;
+
+    @FXML
+    private TextField addEmployee_search;
+
+    @FXML
+    private TableView<?> addEmployee_tableView;
+
+    @FXML
+    private Button addEmployee_updateBtn;
+
+    @FXML
+    private Button close;
+
+    @FXML
+    private Button home_btn;
+
+    @FXML
+    private BarChart<?, ?> home_chart;
+
+    @FXML
+    private AnchorPane home_form;
+
+    @FXML
+    private Label home_totalEmployees;
+
+    @FXML
+    private Label home_totalInactiveEm;
+
+    @FXML
+    private Label home_totalPresents;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Button minimize;
+
+    @FXML
+    private Button salary_btn;
+
+    @FXML
+    private Button salary_clearBtn;
+
+    @FXML
+    private TableColumn<?, ?> salary_col_employeeID;
+
+    @FXML
+    private TableColumn<?, ?> salary_col_firstName;
+
+    @FXML
+    private TableColumn<?, ?> salary_col_lastName;
+
+    @FXML
+    private TableColumn<?, ?> salary_col_position;
+
+    @FXML
+    private TableColumn<?, ?> salary_col_salary;
+
+    @FXML
+    private TextField salary_employeeID;
+
+    @FXML
+    private Label salary_firstName;
+
+    @FXML
+    private AnchorPane salary_form;
+
+    @FXML
+    private Label salary_lastName;
+
+    @FXML
+    private Label salary_position;
+
+    @FXML
+    private TextField salary_salary;
+
+    @FXML
+    private TableView<?> salary_tableView;
+
+    @FXML
+    private Button salary_updateBtn;
+
+    @FXML
+    private Label username;
+
 }
